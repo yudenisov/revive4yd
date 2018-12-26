d2e4a80449204d46b38cb78045a1fc0ee7ffa7e7
Юрий А. Денисов <yudenisov@mail.ru>
2018-12-26 21:02:36 +0400

    version December 26 2018

---
+++ functions-Server-Home.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * revive functions and definitions
+ *
+ * @package revive
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 640; /* pixels */
+}
+
+if ( ! function_exists( 'revive_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function revive_setup() {
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on revive, use a find and replace
+	 * to change 'revive' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'revive', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 *
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', 'revive' ),
+		'bottom' => __( 'Bottom Menu', 'revive' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link',
+	) );
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'revive_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+	
+	add_image_size('revive-pop-thumb',542, 343, true );
+	add_image_size('revive-revive-pop-thumb-half',542, 170, true );
+	add_image_size('revive-poster-thumb',542, 680, true );
+}
+endif; // revive_setup
+add_action( 'after_setup_theme', 'revive_setup' );
+
+/**
+ * Register widget area.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_sidebar
+ */
+function revive_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar', 'revive' ),
+		'id'            => 'sidebar-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title title-font">',
+		'after_title'   => '</h1>',
+	) );
+	
+	register_sidebar( array(
+		'name'          => __( 'Footer 1', 'revive' ), /* Primary Sidebar for Everywhere else */
+		'id'            => 'footer-1',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title title-font">',
+		'after_title'   => '</h1>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer 2', 'revive' ), /* Primary Sidebar for Everywhere else */
+		'id'            => 'footer-2',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title title-font">',
+		'after_title'   => '</h1>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Footer 3', 'revive' ), /* Primary Sidebar for Everywhere else */
+		'id'            => 'footer-3',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title title-font">',
+		'after_title'   => '</h1>',
+	) );
+	
+	register_sidebar( array(
+		'name'          => __( 'Footer 4', 'revive' ), /* Primary Sidebar for Everywhere else */
+		'id'            => 'footer-4',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title title-font">',
+		'after_title'   => '</h1>',
+	) );
+}
+add_action( 'widgets_init', 'revive_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function revive_scripts() {
+	wp_enqueue_style( 'revive-style', get_stylesheet_uri() );
+	
+	wp_enqueue_style('revive-title-font', '//fonts.googleapis.com/css?family='.str_replace(" ", "+", get_theme_mod('revive_title_font', 'Roboto') ).':100,300,400,700' );
+	
+	wp_enqueue_style('revive-body-font', '//fonts.googleapis.com/css?family='.str_replace(" ", "+", get_theme_mod('revive_body_font', 'Roboto') ).':100,300,400,700' );
+	
+	wp_enqueue_style( 'revive-fontawesome-style', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css' );
+	
+	wp_enqueue_style( 'revive-nivo-style', get_template_directory_uri() . '/assets/css/nivo-slider.css' );
+	
+	wp_enqueue_style( 'revive-nivo-skin-style', get_template_directory_uri() . '/assets/css/nivo-default/default.css' );
+	
+	wp_enqueue_style( 'revive-bootstrap-style', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css' );
+	
+	wp_enqueue_style( 'revive-fleximage-style', get_template_directory_uri() . '/assets/css/jquery.flex-images.css' );
+
+	wp_enqueue_style( 'revive-hover-style', get_template_directory_uri() . '/assets/css/hover.min.css' );
+		
+	wp_enqueue_style( 'revive-main-theme-style', get_template_directory_uri() . '/assets/css/main.css' );
+
+	wp_enqueue_style( 'revive-forPrice-theme-style', get_template_directory_uri() . '/assets/css/forPrice.css' );
+
+	wp_enqueue_style( 'revive-Lato-font', get_template_directory_uri() . '/assets/fonts/fontLato.css' );
+
+	wp_enqueue_script( 'revive-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	
+	wp_enqueue_script( 'revive-externaljs', get_template_directory_uri() . '/js/external.js', array('jquery'), '20120206', true );
+
+	wp_enqueue_script( 'revive-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+	
+	wp_enqueue_script( 'revive-custom-js', get_template_directory_uri() . '/js/custom.js' );
+}
+add_action( 'wp_enqueue_scripts', 'revive_scripts' );
+
+/**
+ * Enqueue Scripts for Admin
+ */
+function revive_custom_wp_admin_style() {
+        wp_enqueue_style( 'revive-admin_css', get_template_directory_uri() . '/assets/css/admin.css' );
+}
+add_action( 'admin_enqueue_scripts', 'revive_custom_wp_admin_style' );
+
+/**
+ * Include the Custom Functions of the Theme.
+ */
+require get_template_directory() . '/framework/theme-functions.php';
+
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Implement the Custom CSS Mods.
+ */
+require get_template_directory() . '/inc/css-mods.php';
+
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
